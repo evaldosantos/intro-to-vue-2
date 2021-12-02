7c38c3887136e10c19c6f1e2b5636fa01cfc1b4a
Evaldo Santos <evaldojr2@gmail.com>
2021-12-01 21:42:05 -0300

    Computed Properties

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var app = new Vue({
   el: "#app",
   data: {
+    brand: "Vue Mastery",
     product: "Socks",
-    image: "./images/vmSocks-green-onWhite.jpeg",
-    inStock: true,
+    selectedImage: 0,
     details: [
       "80% cotton",
       "20% polyster",
@@ -14,11 +14,13 @@ var app = new Vue({
         variantId: 2234,
         variantColor: "green",
         variantImage: "./images/vmSocks-green-onWhite.jpeg",
+        variantQuantity: 10
       },
       {
         variantId: 2235,
         variantColor: "blue",
         variantImage: "./images/vmSocks-blue-onWhite.jpeg",
+        variantQuantity: 0
       }
     ],
     cart: 0
@@ -27,8 +29,19 @@ var app = new Vue({
     addToCart() {
       this.cart += 1;
     },
-    updateProduct(variantImage) {
-      this.image = variantImage;
+    updateProduct(index) {
+      this.selectedImage = index;
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product;
+    },
+    image() {
+      return this.variants[this.selectedImage].variantImage;
+    },
+    inStock() {
+      return this.variants[this.selectedImage].variantQuantity > 0;
     }
   }
 });
